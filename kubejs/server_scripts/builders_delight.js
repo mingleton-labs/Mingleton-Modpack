4c70f0eedbbda40e0e74fc55e0d23dcd6a65ab38
Isaac Shea <isaac_shea@icloud.com>
2026-09-06 15:53:56 +1000

    Added custom recipes for builders delight lanterns and chains

--- kubejs/server_scripts/builders_delight.js
+++ kubejs/server_scripts/builders_delight.js
@@ -1,5 +1,27 @@
-// Remove the Builder's Delight Chisel and all its recipes. They are replaced by the Chipped workbenches.
-
 ServerEvents.recipes((event) => {
+  // Remove the Builder's Delight Chisel; all recipes now use the Chipped chisel.
   event.remove({ id: "buildersdelight:iron_chisel" });
+
+  // Add custom recipes for golden, rope and copper chains
+  event.shaped(Item.of("buildersdelight:chain_1", 1), [" A ", " B ", " A "], {
+    A: "minecraft:gold_nugget",
+    B: "minecraft:gold_ingot",
+  });
+  event.shaped(Item.of("buildersdelight:chain_2", 1), [" A ", " A ", " A "], {
+    A: "farmersdelight:rope",
+  });
+  event.shaped(Item.of("buildersdelight:chain_3", 1), [" A ", " B ", " A "], {
+    A: "create:copper_nugget",
+    B: "minecraft:copper_ingot",
+  });
+
+  // Add custom recipes for copper and golden lanterns
+  event.shaped(Item.of("buildersdelight:lantern_3", 1), ["AAA", "ABA", "AAA"], {
+    A: "create:copper_nugget",
+    B: "minecraft:torch",
+  });
+  event.shaped(Item.of("buildersdelight:lantern_7", 1), ["AAA", "ABA", "AAA"], {
+    A: "minecraft:gold_nugget",
+    B: "minecraft:torch",
+  });
 });
