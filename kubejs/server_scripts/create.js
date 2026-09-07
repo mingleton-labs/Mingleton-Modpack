08554c89543e7a36954834632b8e6553d836523d
BenSaunders1 <saunders.benjamin@outlook.com>
2026-09-07 23:05:47 +1000

    Add Create KubeJS addon, and add crushed andesite mixer recipe

--- kubejs/server_scripts/create.js
+++ kubejs/server_scripts/create.js
@@ -1,21 +1,23 @@
 ServerEvents.recipes((event) => {
-    event.remove({mod: "create", output:"create:andesite_alloy"});
-    event.shapeless(Item.of('kubejs:crushed_andesite', 4),
-    [
-        'immersiveengineering:hammer',
-        '4x minecraft:andesite'
-    ]);
-    event.shaped(Item.of('kubejs:wet_andesite_pile', 8),
-    [
-        "CAC",
-        "ABA",
-        "CAC"
-    ],
-    {
-        A: 'kubejs:crushed_andesite',
-        B: 'minecraft:water_bucket',
-        C: 'farmersdelight:canvas'
-    });
+    event.remove({ mod: "create", output: "create:andesite_alloy" });
+    event.shapeless(Item.of('kubejs:crushed_andesite', 2),
+        [
+            'immersiveengineering:hammer',
+            '4x minecraft:andesite'
+        ]);
+    event.shaped(Item.of('kubejs:wet_andesite_pile', 4),
+        [
+            "DAC",
+            "ABA",
+            "CAD"
+        ],
+        {
+            A: 'kubejs:crushed_andesite',
+            B: 'minecraft:water_bucket',
+            C: 'farmersdelight:canvas',
+            D: 'minecraft:iron_nugget'
+        });
     event.smelting('create:andesite_alloy', 'kubejs:wet_andesite_pile');
     event.blasting('create:andesite_alloy', 'kubejs:wet_andesite_pile');
+    event.recipes.create.mixing('kubejs:crushed_andesite', '4x minecraft:andesite');
 });
