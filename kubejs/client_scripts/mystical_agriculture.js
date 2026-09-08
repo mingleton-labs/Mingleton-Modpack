63749efc752dfb671868c9c54453e07ef858688c
Isaac Shea <isaac_shea@icloud.com>
2026-09-08 21:36:33 +1000

    Remove growth accelerators; remove mystical customization

--- kubejs/client_scripts/mystical_agriculture.js
+++ kubejs/client_scripts/mystical_agriculture.js
@@ -1,11 +1,17 @@
 RecipeViewerEvents.removeEntriesCompletely("item", (event) => {
-  // Item entries take an ingredient/predicate, not a recipe filter.
-  // Intersect "any item from the mod" with the armor tag.
+  // Armor & tools
   event.remove(
-    Ingredient.of("@mysticalagriculture").and(Ingredient.of("#c:armors"))
+    Ingredient.of("@mysticalagriculture").and(Ingredient.of("#c:armors")),
+  );
+  event.remove(
+    Ingredient.of("@mysticalagriculture").and(Ingredient.of("#c:tools")),
   );
 
-  event.remove(
-    Ingredient.of("@mysticalagriculture").and(Ingredient.of("#c:tools"))
-  );
+  // Growth accelerators
+  event.remove("mysticalagriculture:inferium_growth_accelerator");
+  event.remove("mysticalagriculture:prudentium_growth_accelerator");
+  event.remove("mysticalagriculture:tertium_growth_accelerator");
+  event.remove("mysticalagriculture:imperium_growth_accelerator");
+  event.remove("mysticalagriculture:supremium_growth_accelerator");
+  event.remove("mysticalagriculture:awakened_supremium_growth_accelerator");
 });
--- kubejs/server_scripts/mystical_agriculture.js
+++ kubejs/server_scripts/mystical_agriculture.js
@@ -1,4 +1,24 @@
 ServerEvents.recipes((event) => {
   event.remove({ mod: "mysticalagriculture", output: "#c:armors" });
-  event.remove({ mod: "mysticalagriculture", output: "#c:tools " });
+  event.remove({ mod: "mysticalagriculture", output: "#c:tools" });
+
+  // Growth accelerators
+  event.remove({
+    id: "mysticalagriculture:inferium_growth_accelerator",
+  });
+  event.remove({
+    id: "mysticalagriculture:prudentium_growth_accelerator",
+  });
+  event.remove({
+    id: "mysticalagriculture:tertium_growth_accelerator",
+  });
+  event.remove({
+    id: "mysticalagriculture:imperium_growth_accelerator",
+  });
+  event.remove({
+    id: "mysticalagriculture:supremium_growth_accelerator",
+  });
+  event.remove({
+    id: "mysticalagriculture:awakened_supremium_growth_accelerator",
+  });
 });
